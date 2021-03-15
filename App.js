47df0419a484a4945dc612fef49d25aec0107e10
monimar <61383537+monimar@users.noreply.github.com>
2021-03-14 22:48:20 -0500

    Video52-Opciones de Usuario

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './navigations/Navigation';
+import { LogBox } from 'react-native'
 
+LogBox.ignoreAllLogs()
 
 export default function App() {
   return (
